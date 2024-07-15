d1625da67d1f7e9e68dee3dcd809c9388233dce9
AstroKeith <77962754+AstroKeith@users.noreply.github.com>
2024-07-15 23:07:30 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,26 +4,25 @@ echo "eFinder install"
 echo "This will take some time! ( > 30 minutes)"
 echo " "
 echo "*****************************************************************************"
-#sudo rpi-update -y
-
+echo "Updating Pi OS & packages"
+echo "*****************************************************************************"
 sudo apt update
 sudo apt upgrade -y
+echo " "
+echo "*****************************************************************************"
+echo "Installing additional Debian and Python packages"
+echo "*****************************************************************************"
+sudo apt install -y python3-pil.imagetk
+sudo apt install -y imagemagick
 
 HOME=/home/efinder
-
+echo " "
+echo "*****************************************************************************"
+echo "Installing new astrometry packages"
+echo "*****************************************************************************"
 sudo apt-get install -y libcairo2-dev libnetpbm11-dev netpbm libpng-dev libjpeg-dev zlib1g-dev libbz2-dev swig libcfitsio-dev
-# sudo -u efinder python3 -m pip install --upgrade pip
-
 sudo apt install -y python3-fitsio
-sudo apt install -y imagemagick
 sudo apt install -y python3-skyfield
-sudo apt install -y python3-pil.imagetk
-
-python -m venv /home/efinder/venv-efinder --system-site-packages
-venv-efinder/bin/python venv-efinder/bin/pip install astropy pyfits
-venv-efinder/bin/python venv-efinder/bin/pip install git+https://github.com/esa/tetra3.git
-
-cd $HOME
 sudo -u efinder git clone https://github.com/dstndstn/astrometry.net.git
 echo ""
 
@@ -33,9 +32,7 @@ make
 make py
 make extra
 sudo make install
-
 sudo sh -c "echo export PATH=$PATH:/usr/local/astrometry/bin >> /etc/profile"
-
 cd /usr/local/astrometry/data
 sudo wget http://data.astrometry.net/4100/index-4107.fits
 sudo wget http://data.astrometry.net/4100/index-4108.fits
@@ -48,6 +45,25 @@ cd /usr/local/astrometry/annotate_data
 sudo wget http://data.astrometry.net/hd.fits
 sudo wget http://data.astrometry.net/hip.fits
 sudo wget https://github.com/dstndstn/astrometry.net/tree/main/catalogs/abell-all.fits
+
+python -m venv /home/efinder/venv-efinder --system-site-packages
+venv-efinder/bin/python venv-efinder/bin/pip install astropy pyfits
+mkdir /home/efinder/Solver
+mkdir /home/efinder/Solver/Stills
+mkdir /home/efinder/Solver/data
+mkdir /home/efinder/Solver/images
+
+echo " "
+echo "*****************************************************************************"
+echo "Downloading Tetra databases"
+echo "*****************************************************************************"
+venv-efinder/bin/python venv-efinder/bin/pip install gdown
+venv-efinder/bin/gdown  --output /home/efinder/Solver/data --folder https://drive.google.com/drive/folders/1uxbdttpg0Dpp8OuYUDY9arYoeglfZzcX
+venv-efinder/bin/python venv-efinder/bin/pip install git+https://github.com/esa/tetra3.git
+cd /home/efinder/venv-efinder/lib/python3.11/site-packages/tetra3
+sudo wget https://cdsarc.u-strasbg.fr/ftp/cats/I/239/hip_main.dat
+
+
 
 cd $HOME
 
@@ -72,9 +88,6 @@ cd $HOME
 echo "tmpfs /var/tmp tmpfs nodev,nosuid,size=100M 0 0" | sudo tee -a /etc/fstab > /dev/null
 
 
-mkdir /home/efinder/Solver
-mkdir /home/efinder/Solver/Stills
-
 sudo apt install -y samba samba-common-bin
 sudo tee -a /etc/samba/smb.conf > /dev/null <<EOT
 [efindershare]
@@ -92,6 +105,8 @@ sudo systemctl restart smbd
 echo ""
 cp /home/efinder/eFinder64/Solver/*.* /home/efinder/Solver
 cp /home/efinder/eFinder64/Solver/de421.bsp /home/efinder
+cp /home/efinder/eFinder_Lite/Solver/starnames.csv /home/efinder/Solver/data
+cp /home/efinder/eFinder_Lite/Solver/generate_database.py /home/venv-efinder/lib/python3.11/site-packages/tetra3
 
 mkdir /home/efinder/.config/autostart
 cp /home/efinder/eFinder64/efinder.desktop /home/efinder/.config/autostart
